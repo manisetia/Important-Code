ca565ed1d44fb489b909d470683b6b3016f7dffc
manisetia <97230411+manisetia@users.noreply.github.com>
2022-04-09 18:41:01 +0530

    Create Shopify-variables-for-public-app.php
    Shopify Variables to get current customer id or current page detail in one function with javascript

---
+++ Shopify-variables-for-public-app.php
@@ -0,0 +1,33 @@
+ShopifyAnalytics.meta.page
+-------------------------------------------------
+window.ShopifyAnalytics.meta.page.customerId;
+
+
+
+var getCustomerId = function() {
+    try {
+      let curr = window.ShopifyAnalytics.meta.page.customerId;
+      if (curr !== undefined && curr !== null && curr !== "") {
+        return curr;
+      }
+    } catch(e) { }
+    try {
+      let curr = window.meta.page.customerId;
+      if (curr !== undefined && curr !== null && curr !== "") {
+        return curr;
+      }
+    } catch (e) { }    
+    try {
+      let curr = _st.cid;
+      if (curr !== undefined && curr !== null && curr !== "") {
+        return curr;
+      }
+    } catch (e) { }
+    try {
+      let curr = ShopifyAnalytics.lib.user().traits().uniqToken;
+      if (curr !== undefined && curr !== null && curr !== "") {
+        return curr;
+      }
+    } catch (e) { }
+    return null;
+  }
