e350e041eb0dff1e47ec1f5657c0d944a0dd2fbb
manisetia <97230411+manisetia@users.noreply.github.com>
2022-04-10 12:59:28 +0530

    Update shopify-product-search-by-skus-with-graphql-api.php

--- shopify-product-search-by-skus-with-graphql-api.php
+++ shopify-product-search-by-skus-with-graphql-api.php
@@ -1,9 +1,11 @@
-curl -X POST \
-  https://f144e10537940291d73c0eef308203f1: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@local-primary-store.myshopify.com/admin/api/unstable/graphql.json \
-  -H 'content-type: application/graphql' \
-  -H 'x-shopify-access-token: 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' \
-  -d '{
-    products(first: 5, query: "(sku:MAN123) OR (sku:123SE)") {
+<?php
+
+$url = "https://f144e10537940291d73c0eef308203f1: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@local-primary-store.myshopify.com/admin/api/unstable/graphql.json";
+
+var_dump($url);
+/* Member Metafields Shopify */
+$graphql_data = '{
+products(first: 5, query: "(sku:MAN123) OR (sku:123SE)") {
       edges {
         node {
           id
@@ -14,4 +16,39 @@ curl -X POST \
         hasNextPage
       }
     }
-  }'
+  }';
+//Use CURL to send the data	
+//open connection
+$ch = curl_init();
+		
+//set the url, number of GET vars, GET data
+curl_setopt($ch,CURLOPT_URL, $url);
+
+//Set the curl to POST, not GET	
+curl_setopt($ch,CURLOPT_POST, 1);
+
+//Use the php function http_build_query to create a data array to be send with your metadata
+curl_setopt($ch,CURLOPT_POSTFIELDS, $graphql_data)
+;
+
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'content-type: application/graphql',
+    'x-shopify-access-token: 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'
+));
+
+
+//Set your Shopify login tokens
+	
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+$get_data = json_decode($result,true);
+//$member_id = $get_data['metafield']['id'];
+
+/* End Member Metafields Shopify */
+
+var_dump($get_data);
+
+
+?>
